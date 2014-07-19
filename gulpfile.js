5d80f9a2e8d9ea3b7b7753ccc09baa59a7a4ee41
Chris J. Lebron <chris@chrisjlebron.com>
2014-07-19 16:07:29 -0400

    Add gulpfile template

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+// var watcher = gulp.watch(['library/js/**/*.js','library/scss/**/*.scss'], ['uglify','reload']);
+// watcher.on('change', function(event) {
+//   console.log('File '+event.path+' was '+event.type+', running tasks...');
+// });

44f72daba6a7c4c456590ab36d1de4c8bf903fc4
Chris J. Lebron <chris@chrisjlebron.com>
2014-07-26 14:53:50 -0400

    Add livereload to gulp; Change references to assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     jshint = require('gulp-jshint'),
+    livereload = require('gulp-livereload'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify');
 
 gulp.task('lint-js', function() {
-  return gulp.src('library/js/*.js')
+  return gulp.src(['library/js/**/*.js','!library/js/libs/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('minify-js', function () {
-  return gulp.src('library/js/*.js')
+  return gulp.src(['library/js/**/*.js', '!library/js/libs/modernizr.custom.min.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('build/js'))
     .pipe(rename('app.min.js'))
@@ -20,26 +21,29 @@ gulp.task('minify-js', function () {
     .pipe(gulp.dest('build/js'));
 });
 
+gulp.task('modernizr', function () {
+  return gulp.src('library/js/libs/**/*.js')
+    .pipe(gulp.dest('build/js/libs'))
+});
+
 gulp.task('sass', function () {
-  return gulp.src('library/scss/*.scss')
+  return gulp.src('library/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('build/css'));
 });
 
 
 gulp.task('watch', function () {
-  gulp.watch('library/js/*.js', ['lint-js', 'minify-js']);
-  gulp.watch('library/scss/*.scss', ['sass']);
+  livereload.listen();
+  gulp.watch('library/js/**/*.js', ['lint-js', 'minify-js']);
+  gulp.watch('library/scss/**/*.scss', ['sass']);
+  gulp.watch('build/**/*').on('change', livereload.changed);
 });
 
 gulp.task('default',[
+  'modernizr',
   'lint-js',
-  'sass',
   'minify-js',
+  'sass',
   'watch'
 ]);
-
-// var watcher = gulp.watch(['library/js/**/*.js','library/scss/**/*.scss'], ['uglify','reload']);
-// watcher.on('change', function(event) {
-//   console.log('File '+event.path+' was '+event.type+', running tasks...');
-// });

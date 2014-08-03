bf7a6df8044ad913ea244a9ba9f897d9faa9a81c
Chris J. Lebron <chris@chrisjlebron.com>
2014-08-03 16:29:37 -0400

    Add custom fonts, config headings

--- functions.php
+++ functions.php
@@ -199,7 +199,7 @@ can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
 function bones_fonts() {
-  wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+  wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Patua+One|Open+Sans:400,700,400italic,700italic');
   wp_enqueue_style( 'googleFonts');
 }
 
